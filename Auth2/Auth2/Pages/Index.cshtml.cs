20400ff0a4601556f2c0b1d356bd14c56bc39458
Nicholas Ho <terran324@gmail.com>
2022-07-03 16:07:20 +1000

    create Razor pages project, create login page

---
+++ Auth2/Auth2/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Auth2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            // At this stage, we have an anonymous identity. 
+            // Watching base.User.Identity (The primary identity), we can see that there are no claims, and IsAuthenticated is false
+            // because we have not yet logged in.
+            // asp.net creates the primary identity for us, regardless of whether or not we have logged in.
+            // i.e. we have a security context, albeit an anonymous (empty) one.
+        }
+    }
+}
